e6c415c7162922e974b4409cc6723117c06f582f
Davis <ho861206@gmail.com>
2018-11-23 11:37:02 +0800

    insert data base and rename

---
+++ PHP Tutorial/Insert database data/View.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DataBaseView</title>
+</head>
+<body>
+<form action = "include/signup.inc.php" method="POST">
+	<input type="text" name="first" placeholder="Firstname">
+	<br>
+	<input type="text" name="last" placeholder="LastName">
+	<br>
+	<input type="text" name="email" placeholder="E-mail">
+	<br>
+	<input type="text" name="uid" placeholder="Username">
+	<br>
+	<input type="password" name="first" placeholder="Password">
+	<br>
+	<button type="submit" name = "submit">Sign up</button>
+</form>
+
+</body>
+</html>
