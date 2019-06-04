e773234551da7c898bba96752ceca814042faff5
Daviswww <ho861206@gmail.com>
2019-06-04 16:11:23 +0800

    php tutorial

---
+++ PHP Tutorial/EXC 008 - OOP Class/index.php
@@ -0,0 +1,20 @@
+<?php
+    include 'includes/newclass.inc.php';
+    include 'includes/parentclass.inc.php';
+    $object = new NewClass;
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            include!
+        </title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>
+            Hello World!
+        </p>
+    </body>
+</html>
